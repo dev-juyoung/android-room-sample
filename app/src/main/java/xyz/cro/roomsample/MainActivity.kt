51a50f4f274e115728f3a159031e4bad0b917c53
dev.juyoung <dev.juyoung@gmail.com>
2018-03-14 16:59:34 +0900

    * Room을 이용한 insert / query 처리 테스트.

--- app/src/main/java/xyz/cro/roomsample/MainActivity.kt
+++ app/src/main/java/xyz/cro/roomsample/MainActivity.kt
@@ -1,12 +1,37 @@
 package xyz.cro.roomsample
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val database: UserDatabase by lazy { UserDatabase.getInstance(this) }
+    private val dao: UserDao by lazy { database.userDao() }
+
+    private val appExecutor: AppExecutors by lazy { AppExecutors() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonAddUser.setOnClickListener {
+            val user: User = User(1, "Juyoung", "Lee")
+
+            appExecutor.diskIO.execute {
+                dao.add(user)
+
+                appExecutor.mainThread.execute { getUserList() }
+            }
+        }
+    }
+
+    private fun getUserList() {
+        appExecutor.diskIO.execute {
+            val users = dao.getAll()
+
+            appExecutor.mainThread.execute { Log.d("ROOM-DATABASE", "users: $users") }
+        }
     }
 }
